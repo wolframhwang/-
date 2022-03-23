b3d32ebb86541197a5d3d7e6a98e73183d02c0bf
Jiwoong Hwang <hzw94@naver.com>
2022-03-23 10:28:20 +0900

    Update Wolfram_로봇 조종하기.swift

--- 로봇 조종하기/Wolfram_로봇 조종하기.swift
+++ 로봇 조종하기/Wolfram_로봇 조종하기.swift
@@ -20,7 +20,9 @@ func makeDP() {
 func solve() {
     dp[1][0][0] = map[0][0]
     dp[0][0][0] = map[0][0]
-
+    for i in 1..<M {
+		dp[0][0][i] = dp[0][0][i - 1] + map[0][i];
+	}
     for i in 1..<N {
         dp[1][i][0] = max(dp[0][i - 1][0], max(dp[1][i - 1][0], dp[2][i - 1][0])) + map[i][0];
 
